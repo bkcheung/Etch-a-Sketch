7f0ec7dbfca9eeba44f54deb07e5471b85d49ace
bkcheung <b.cheung@hotmail.com>
2021-12-31 14:59:50 -0500

    Update formatting, clean up code

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,17 +1,26 @@
+function createGridStyle(numSquares){
+    const styleElement = document.querySelector('#gridStyle');
+    let styleContent = `.gridContainer { display: grid; max-width: 500px; grid-template-columns: repeat(${numSquares}, 1fr);}`;
+    styleElement.textContent = styleContent;
+}
+
 function createDiv(){
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('squareDiv');
     container.append(squareDiv);
 }
 
-function createDivGrid(){
-    for(i=0; i<16; i++){ createDiv();
-        for(j=0; j<16; j++){ createDiv();}
+function createGrid(numSquares){
+    createGridStyle(numSquares);
+    for(i=0; i<numSquares; i++){ 
+        for(j=0; j<numSquares; j++){
+            createDiv();}
     }
 }
 
 function hover(element, className){
     element.addEventListener('mouseenter', e => element.classList.add(className));
+    element.addEventListener('touchstart', e => element.classList.add(className));
 }
 
 function etchSketch(){
@@ -22,5 +31,17 @@ function etchSketch(){
 }
 
 const container = document.querySelector('.gridContainer');
-createDivGrid();
+const clear = document.querySelector('#clear');
+let numSquares = 0;
+createGrid(16);
 etchSketch();
+
+clear.addEventListener('click', ()=>{
+    numSquares = 0;
+    container.textContent = "";
+    while(numSquares===0 || numSquares>100 || numSquares < 0){
+        numSquares = window.prompt('Enter number of squares per side (1 to 100)');
+    }
+    createGrid(numSquares);
+    etchSketch();
+})
