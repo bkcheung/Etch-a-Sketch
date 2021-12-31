93418a1c0b96820502f8c79c4d90bb915b671c19
bkcheung <b.cheung@hotmail.com>
2021-12-30 22:04:56 -0500

    Add draw-on-hover function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,17 +2,25 @@ function createDiv(){
     const squareDiv = document.createElement('div');
     squareDiv.classList.add('squareDiv');
     container.append(squareDiv);
-    return
 }
 
 function createDivGrid(){
-    for(i=0; i<16; i++){
-        createDiv();
-        for(j=0; j<16; j++){
-            createDiv();
-        }
+    for(i=0; i<16; i++){ createDiv();
+        for(j=0; j<16; j++){ createDiv();}
     }
+}
+
+function hover(element, className){
+    element.addEventListener('mouseenter', e => element.classList.add(className));
+}
+
+function etchSketch(){
+    const squareDiv = document.querySelectorAll('.squareDiv');
+    squareDiv.forEach(div => {
+        hover(div, 'hovered');
+    });
 }
 
 const container = document.querySelector('.gridContainer');
 createDivGrid();
+etchSketch();
